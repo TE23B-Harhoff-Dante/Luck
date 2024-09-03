bf698fb5583a62ce199561cc5b6905ce24c6ccbc
Dante Hahroff <dante.harhoff@elev.ga.ntig.se>
2024-09-03 13:27:15 +0200

    yibiuliulb

---
+++ LuckText/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+string name = "";
+string videoGame = "";
+string animal = ""; 
+
+Console.WriteLine("skriv ditt favorit spel");
+videoGame = Console.ReadLine();
+
+Console.WriteLine("\nskriv en djur");
+animal = Console.ReadLine();
+
+Console.WriteLine("\nvad heter du\n");
+name = Console.ReadLine();
+
+Console.WriteLine($"Så du heter " + name + " och gillar " + videoGame + " då är du en " + animal );
+
+Console.ReadLine();
